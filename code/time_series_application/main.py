e8c2a48bc892c291f38617e316fd31af6c96ce52
benjaminbluhm <benjaminbluhm@gmx.de>
2019-04-28 13:06:55 +0200

    Activate non-parallel execution

--- code/time_series_application/main.py
+++ code/time_series_application/main.py
@@ -21,13 +21,13 @@ def main():
     partition_and_save_dataset(spark_session, config)
 
     # Perform non-parallel model fitting and forecasting
-    # do_non_parallel_forecasting(spark_session, config, cloud=cloud_modus)
+    do_non_parallel_forecasting(spark_session, config, cloud=cloud_modus)
 
-    # # Add Python module to Spark context for distributed model fitting and forecasting
-    spark_session.sparkContext.addPyFile(config['base_path_hadoop'] + 'fit_model_and_forecast.py')
-
-    # Perform parallel model fitting and forecasting
-    do_parallel_forecasting(spark_session, config, cloud=cloud_modus)
+    # # # Add Python module to Spark context for distributed model fitting and forecasting
+    # spark_session.sparkContext.addPyFile(config['base_path_hadoop'] + 'fit_model_and_forecast.py')
+    #
+    # # Perform parallel model fitting and forecasting
+    # do_parallel_forecasting(spark_session, config, cloud=cloud_modus)
 
 if __name__ == '__main__':
     main()
