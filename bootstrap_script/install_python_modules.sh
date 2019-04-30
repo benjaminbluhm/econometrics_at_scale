8aaf076d42bc5cc97c00762756a0f67582c02640
benjaminbluhm <benjaminbluhm@gmx.de>
2019-04-30 19:42:49 +0200

    Modified AWS bootstrap action

--- bootstrap_script/install_python_modules.sh
+++ bootstrap_script/install_python_modules.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -xe
 
+# Repeatedly remove older numpy version from cluster (otherwise fastparquet import will fail)
+sudo pip install -U numpy
+sudo pip install -U numpy
+
 # Install Python modules:
 sudo pip install -U \
+  numpy             \
   pandas            \
   scipy				\
   fastparquet		\
   s3fs				\
   s3io				\
   joblib	\
+  findspark \
   statsmodels
